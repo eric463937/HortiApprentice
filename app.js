32c6da6fff27b70703477244b2a2f7c13d8591a0
eric463937 <eric463937@gmail.com>
2016-08-19 18:02:46 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ $(document).ready(function () {
     console.log("ready!");
     $("#nav_box").load("nav.html");
 });
+
+
+$(document).ready(function () {
+    console.log("ready!");
+    $("#footer_box").load("footer.html");
+});
